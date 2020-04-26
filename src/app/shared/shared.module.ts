55a650e0868df88f137042b7c6fd984cd87410ab
shivani kohli <shivani.kohli99k@gmail.com>
2020-04-26 05:46:07 +0530

    changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from '@material/material.module';
+import { MaterialModule } from '../material/material.module';
 import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
